abc3725e886326ff17be4577a617f37587b38653
belugawhalesarecool <jasev614@gmail.com>
2025-03-02 23:34:17 -0500

    name intro animation

--- Real website/Html/standard format/embêr.js
+++ Real website/Html/standard format/embêr.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', () => {
-    const title = document.querySelector('.name');
+    const title = document.querySelector('.name-container');
     const scrollY = window.scrollY; 
     const fadePoint = 250; 
     let opacity = 1 - scrollY / fadePoint;
@@ -7,14 +7,6 @@ window.addEventListener('scroll', () => {
     title.style.opacity = opacity;
 });
 
-window.addEventListener('scroll', () => {
-    const title = document.querySelector('.undername');
-    const scrollY = window.scrollY; 
-    const fadePoint = 250; 
-    let opacity = 1 - scrollY / fadePoint;
-    opacity = Math.max(opacity, 0);
-    title.style.opacity = opacity;
-});
 const swiper = new Swiper('.swiper', {
     loop: true,
     autoplay: {
