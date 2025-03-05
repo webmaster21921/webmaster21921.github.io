b2fba8d29d5c09e34a1e7e54524fc4a5c622c870
belugawhalesarecool <jasev614@gmail.com>
2025-03-05 00:35:05 -0500

    more changes to color

---
+++ Real website/Html/standard format/embêr.js
@@ -0,0 +1,30 @@
+window.addEventListener('scroll', () => {
+  const title = document.querySelector('.name');
+  const scrollY = window.scrollY; 
+  const fadePoint = 250; 
+  let opacity = 1 - scrollY / fadePoint;
+  opacity = Math.max(opacity, 0);
+  title.style.opacity = opacity;
+});
+
+window.addEventListener('scroll', () => {
+const title = document.querySelector('.name-container');
+const scrollY = window.scrollY; 
+const fadePoint = 250; 
+let opacity = 1 - scrollY / fadePoint;
+opacity = Math.max(opacity, 0);
+title.style.opacity = opacity;
+});
+
+const swiper = new Swiper('.swiper', {
+  speed: 3000,
+  loop: true,
+  autoplay: {
+    delay: 3000,               
+    disableOnInteraction: false // Continues autoplay after user interacts
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
