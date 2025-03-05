ada5f564dbb6f76b2298c0ba447bff0ec781986c
belugawhalesarecool <jasev614@gmail.com>
2025-03-04 22:00:14 -0500

    fixed fade away animation
    z-index of name-container was too low

--- Real website/Html/standard format/embêr.js
+++ Real website/Html/standard format/embêr.js
@@ -1,10 +1,19 @@
 window.addEventListener('scroll', () => {
-    const title = document.querySelector('.name-container');
+    const title = document.querySelector('.name');
     const scrollY = window.scrollY; 
     const fadePoint = 250; 
     let opacity = 1 - scrollY / fadePoint;
     opacity = Math.max(opacity, 0);
     title.style.opacity = opacity;
+});
+
+window.addEventListener('scroll', () => {
+  const title = document.querySelector('.name-container');
+  const scrollY = window.scrollY; 
+  const fadePoint = 250; 
+  let opacity = 1 - scrollY / fadePoint;
+  opacity = Math.max(opacity, 0);
+  title.style.opacity = opacity;
 });
 
 const swiper = new Swiper('.swiper', {
