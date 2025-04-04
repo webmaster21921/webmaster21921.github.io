b84544431f411f96838d1b22d3d033c6905b33bd
belugawhalesarecool <jasev614@gmail.com>
2025-04-04 11:37:52 -0400

    fixed issuees with index

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ images.forEach(img => {
     
     if (textEl && textEl.classList.contains('scrl-text')) {
       
-      // Remove 'show' from any other open .scrl-text
+      
       document.querySelectorAll('.scrl-text.show').forEach(otherText => {
         if (otherText !== textEl) {
           otherText.classList.remove('show');
         }
       });
 
-      // Toggle 'show' on the clicked one
+      
       textEl.classList.toggle('show');
     }
   });
